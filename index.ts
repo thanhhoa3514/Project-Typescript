9616e8bf672573780da625f72d67bb31f705c9e9
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-12-01 13:01:22 +0700

    Set up

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Starting");
+
+import express,{Express,Request,Response} from "express";
+import dotenv from "dotenv"
+import * as database from "./config/database";
+import Task from "./models/task.model";
+
+
+dotenv.config();
+database.connect();
+const app:Express = express();
+const port:string|number = process.env.PORT||3000;
+app.get("/task", async (req:Request, res:Response) => {
+    const tasks = await Task.find({
+        deleted: false,
+    });
+    console.log(tasks);
+    res.json(tasks);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ models/task.model.ts
@@ -0,0 +1,37 @@
+
+// const mongoose = require("mongoose");
+import mongoose from "mongoose";
+
+
+const taskSchema = new mongoose.Schema(
+  {
+    title:String,
+    status:String,
+    content:String,
+    timeStart:Date,
+    timeFinish: Date,
+    // priority: String,
+    createdBy: String,
+    // updatedBy: String,
+    // deletedBy: String,
+    listUser:Array,
+    // comments: Array,
+    // attachments: Array,
+    // tags: Array,
+    // attachments: Array,
+    // notifications: Array,
+    taskParentId:String,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+const Task = mongoose.model("Task", taskSchema, "tasks");
+// console.log(productSchema);
+
+export default Task;
