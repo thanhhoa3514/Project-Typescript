71386bd0e594fb8fb815436771aec0f5ad1127d0
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-12-01 20:21:23 +0700

    Route private

--- api/v1/routes/index.route.ts
+++ api/v1/routes/index.route.ts
@@ -2,10 +2,11 @@
 import { Express } from "express";
 import { taskRoutes } from "./task.route";
 import { userRoutes } from "./user.route";
+import * as middleware from "../middlewares/auth.middleware";
 
 const mainV1Routes = (app:Express):void => {
   const version = "/api/v1";
-  app.use(version + "/tasks", taskRoutes);
+  app.use(version + "/tasks",middleware.requireAuth, taskRoutes);
   app.use(version + "/users", userRoutes);
 };
 export default mainV1Routes;
